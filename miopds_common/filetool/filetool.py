6f616760aeb9082c023328dc6371be80e35ca548
Atsuki Shinbori <shinbori@isee.nagoya-u.ac.jp>
2025-12-02 13:07:54 +0900

    Add file stamp function in filetool.py

--- miopds_common/filetool/filetool.py
+++ miopds_common/filetool/filetool.py
@@ -1,7 +1,8 @@
 import os
 import hashlib
+from datetime import datetime
 
-def get_file_info(filepath: str) -> dict:
+def get_file_info(file_path: str) -> dict:
     """
     Return the file size and md5checksum value of the selected file with dictionary.
 
@@ -14,13 +15,19 @@ def get_file_info(filepath: str) -> dict:
         filepath (str): file path
     
     Returns:
-        dict: {"size": file size (byte), "md5": MD5 (string)}
+        dict: {"creation_time": creation time (ISO format), "size": file size (byte), "md5": MD5 (string)}
     """
-    if not os.path.isfile(filepath):
-        raise FileNotFoundError(f"File not found: {filepath}")
+    if not os.path.isfile(file_path):
+        raise FileNotFoundError(f"File not found: {file_path}")
 
     # Get file size
-    size = os.path.getsize(filepath)
+    size = os.path.getsize(file_path)
+
+    # Get file stamp
+    file_stat = os.stat(file_path)
+    file_stamp = file_stat.st_ctime
+    creation time = datetime.fromtimestamp(file_stamp).isoformat() # ISO format
+
 
     # Get md5 chksum value for the input file
     md5_hash = hashlib.md5()
@@ -29,6 +36,7 @@ def get_file_info(filepath: str) -> dict:
             md5_hash.update(chunk)
 
     return {
+        "creation time": creation time,
         "size": size,
         "md5": md5_hash.hexdigest()
     }
